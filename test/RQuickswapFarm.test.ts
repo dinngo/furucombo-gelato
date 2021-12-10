ad7d06752fa9886e5d6fe92266860a931d4f1c04
Mussina Lin <mussina@dinngo.co>
2021-12-10 14:12:34 +0800

    feat: update test name...

--- test/RQuickswapFarm.test.ts
+++ test/RQuickswapFarm.test.ts
@@ -6,7 +6,7 @@ import { IDSProxy } from "../typechain";
 
 const gelatoAddress = "0x3CACa7b48D0573D793d3b0279b5F0029180E83b6";
 
-describe("TaskTimer", function () {
+describe("RQuickswapFarm", function () {
   this.timeout(0);
   let user0: SignerWithAddress;
   let owner: SignerWithAddress;
